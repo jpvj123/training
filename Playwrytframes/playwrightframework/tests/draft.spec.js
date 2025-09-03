2aa64e43e105ea60ce072b982670f87d28092aba
jeyapraveenvj <praveenvj27051997@gmail.com>
2025-09-03 22:08:20 +0530

    first commit

---
+++ Playwrytframes/playwrightframework/tests/draft.spec.js
@@ -0,0 +1,11 @@
+
+
+await page.goto("https://rahulshettyacademy.com/client");
+ await page.waitForSelector('#userEmail');
+   await page.locator("#userEmail").fill(email);
+   await page.locator("#userPassword").fill("Iamking@000");
+   await page.locator("[value='Login']").click();
+   await page.waitForLoadState('networkidle');
+   await page.locator(".card-body b").first().waitFor();
+   const titles = await page.locator(".card-body b").allTextContents();
+   console.log(titles); 
